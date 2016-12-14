b43d902861f07f17884e9f0b07b929b925a0dd9b
Bader <bader.nmb@Gmail.com>
2016-12-14 13:02:01 -0600

    Write comments

--- Google Apps Script.vb
+++ Google Apps Script.vb
@@ -1,5 +1,5 @@
 
-
+// getting first empty row in column
 function getFirstEmptyRow() {
   var spr = SpreadsheetApp.getActiveSpreadsheet();
   var column = spr.getRange('C:C');
@@ -11,6 +11,61 @@ function getFirstEmptyRow() {
   return (ct);
 }
 
+// this function gets the last level of water then copy it to another column
+// not uesed anymore
+function progress()
+{ 
+
+  var sheet = SpreadsheetApp.getActiveSheet();
+  
+  var distanceCell = sheet.getRange(2,1);  
+  var distanceValue=distanceCell.getValue();
+  
+  var topCell = sheet.getRange(2,2);  
+  var topValue=topCell.getValue();
+  
+  var oldCell=sheet.getRange(getFirstEmptyRow(),3);
+  var oldDistacne=oldCell.getValue();
+  
+  if(topValue =="Closed" && oldDistacne !=distanceValue)
+  {
+  var cell = sheet.getRange(getFirstEmptyRow()+1,3);
+   cell.setValue(distanceValue);
+    target();
+  }
+  
+  }
+
+// this function caluclates how far you are from target.
+// not used anymore
+function target(){
+ 
+  var sheet = SpreadsheetApp.getActiveSheet();
+  
+  var distanceCell = sheet.getRange(getFirstEmptyRow(),3);  
+  var distanceValue=distanceCell.getValue();
+ 
+  var oldCell=sheet.getRange(getFirstEmptyRow()-1,3);
+  var oldDistacne=oldCell.getValue();
+  
+  var cell = sheet.getRange(2,4);
+  var oldTargetValue=cell.getValue();
+  
+  var cell1 = sheet.getRange(2,5);
+  var oldCell1=cell1.getValue();
+  
+  
+  if ((distanceValue<oldDistacne) && (distanceValue !=oldCell1))
+  {
+  var newTarget=oldTargetValue + (oldDistacne - distanceValue);
+    cell1.setValue(distanceValue);
+    cell.setValue(newTarget);
+    
+  }
+}
+ 
+// this function runs at mid night to set the progressinto 0 and runs updateDailyTarget()function
+
   function Clean(){
     
   var sheet = SpreadsheetApp.getActiveSheet();
@@ -20,23 +75,14 @@ function getFirstEmptyRow() {
     
     updateDailyTarget();
     
-  var target = sheet.getRange(2,4);  
-    target.setValue(0)
+  var progress = sheet.getRange(2,3);  
+    progress.setValue(0)
     
-    var lastSteep = sheet.getRange(2,5);  
-    lastSteep.setValue("");
- 
-  while ( values[ct][0] != "" ) {
-    
-    var oldCell=sheet.getRange(ct + 1,3);
-     oldCell.setValue("");
-    
-    ct++;
-} 
+
   
   }
 
-
+// this function updates the chart history 
 function updateDailyTarget(){
  
    var sheet = SpreadsheetApp.getActiveSheet();
@@ -60,3 +106,5 @@ function updateDailyTarget(){
  targetForHistory.setValue(oldTargetValue);
   
 }
+
+
